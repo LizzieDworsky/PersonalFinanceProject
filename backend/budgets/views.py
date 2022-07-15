52760c18dbd488e774a2e5dc56f6ebcef23c9ba6
LizzieDworsky <amydworsky15@gmail.com>
2022-07-15 15:03:37 -0600

    made changes the the budget model's view function change_user_budgets, still working on the PUT endpoint

--- backend/budgets/views.py
+++ backend/budgets/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from categories.models import Category
-from categories.serializers import CategorySerializer
 from .models import Budget
 from .serializer import BudgetSerializer
 
@@ -24,15 +23,21 @@ def get_user_budgets(request):
     return Response(serializer.data)
 
 
-@api_view(["POST"])
+@api_view(["POST", "PUT"])
 @permission_classes([IsAuthenticated])
 def change_user_budgets(request, pk):
     budget_category = get_object_or_404(Category, pk=pk)
-    serializer = BudgetSerializer(data=request.data)
-    if serializer.is_valid():
-        if request.method == "POST":
+    if request.method == "POST":
+        serializer = BudgetSerializer(data=request.data)
+        if serializer.is_valid():
             serializer.save(user=request.user, category=budget_category)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        elif request.method == "PUT":
-            pass
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "PUT":
+        user_budget = Budget.objects.filter(user_id=request.user.id)
+        budget_line = user_budget.filter(category_id=pk)
+        serializer = BudgetSerializer(budget_line, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
