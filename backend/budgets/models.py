dafb03acedec2798088d5d7c408c800404f9a184
LizzieDworsky <amydworsky15@gmail.com>
2022-07-15 16:11:15 -0600

    cleaned up a couple of messy pieces from today's code including adding fields to serializers that had been missed

--- backend/budgets/models.py
+++ backend/budgets/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from authentication.models import User
 from categories.models import Category
@@ -9,5 +10,5 @@ class Budget(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     dollar_amount = models.DecimalField(max_digits=8, decimal_places=2)
     personal_percent = models.PositiveSmallIntegerField(blank=True)
-    date = models.DateField()
+    date = models.DateField(default=datetime.date.today)
     depth = 1
--- backend/budgets/serializer.py
+++ backend/budgets/serializer.py
@@ -5,5 +5,5 @@ from .models import Budget
 class BudgetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Budget
-        fields = ["id", "user_id", "category_id", "category", "dollar_amount", "personal_percent"]
+        fields = ["id", "user_id", "category_id", "category", "dollar_amount", "personal_percent", "date"]
         depth = 1
