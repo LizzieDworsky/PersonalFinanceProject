2e373d623b41b3df43be34eccb2ca2e23907c6bb
LizzieDworsky <amydworsky15@gmail.com>
2022-07-16 09:33:23 -0600

    created a simple unprotected get all endpoint for savings_investments, tested

--- backend/savings_investments/urls.py
+++ backend/savings_investments/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from savings_investments import views
 
 urlpatterns = [
-    path("", views.get_all_savings_investments)
+    path("all", views.get_all_savings_investments)
 ]
--- backend/savings_investments/views.py
+++ backend/savings_investments/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import SavingInvestment
+from .serializers import SavingInvestmentSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_savings_investments(request):
+    saves_invests = SavingInvestment.objects.all()
+    serializer = SavingInvestmentSerializer(saves_invests, many=True)
+    return Response(serializer.data)
