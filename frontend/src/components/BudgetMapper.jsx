9c737dc6d71a44c7a7ea5920180a1d805bd74598
LizzieDworsky <amydworsky15@gmail.com>
2022-07-18 15:42:18 -0600

    working on adding PUT request to budget lines

--- frontend/src/components/BudgetMapper.jsx
+++ frontend/src/components/BudgetMapper.jsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BudgetMapper = ({ budgetsArray }) => {
+    const [updateBool, setUpdateBool] = useState(false);
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Categories</th>
-                    <th>Dollar Amount</th>
-                    <th>Budgeted %</th>
-                    <th>Recommended %</th>
-                </tr>
-            </thead>
-            <tbody>
-                {budgetsArray.map((item) => {
-                    return (
-                        <tr key={item.id}>
-                            <td>{item.category.name}</td>
-                            <td>{item.dollar_amount}</td>
-                            <td>{item.personal_percent}</td>
-                            <td>{item.category.recommended_percent}</td>
+        <div>
+            {updateBool ? null : (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Categories</th>
+                            <th>Dollar Amount</th>
+                            <th></th>
+                            <th>Budgeted %</th>
+                            <th>Recommended %</th>
                         </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+                    </thead>
+                    <tbody>
+                        {budgetsArray.map((item) => {
+                            return (
+                                <tr key={item.id}>
+                                    <td>{item.category.name}</td>
+                                    <td>{item.dollar_amount}</td>
+                                    <td
+                                        onClick={() =>
+                                            setUpdateBool(!updateBool)
+                                        }
+                                    >
+                                        Update
+                                    </td>
+                                    <td>{item.personal_percent}</td>
+                                    <td>{item.category.recommended_percent}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            )}
+        </div>
     );
 };
 
