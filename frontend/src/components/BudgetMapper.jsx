e70b11b5feae9bc941a5366ef9532c8b42a8e7e0
LizzieDworsky <amydworsky15@gmail.com>
2022-07-22 12:19:58 -0600

    refactored Budget Mapper to have a single update button

--- frontend/src/components/BudgetMapper.jsx
+++ frontend/src/components/BudgetMapper.jsx
@@ -13,36 +13,43 @@ const BudgetMapper = ({ budgetsArray, income, axiosUpdateBudget }) => {
                     setUpdateBool={setUpdateBool}
                 />
             ) : (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Categories</th>
-                            <th>Dollar Amount</th>
-                            <th></th>
-                            <th>Budgeted %</th>
-                            <th>Recommended %</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {budgetsArray.map((item) => {
-                            return (
-                                <tr key={item.id}>
-                                    <td>{item.category.name}</td>
-                                    <td>{item.dollar_amount}</td>
-                                    <td
-                                        onClick={() =>
-                                            setUpdateBool(!updateBool)
-                                        }
-                                    >
-                                        Update
-                                    </td>
-                                    <td>{item.personal_percent}</td>
-                                    <td>{item.category.recommended_percent}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Categories</th>
+                                <th>Dollar Amount</th>
+                                {/* <th></th> */}
+                                <th>Budgeted %</th>
+                                <th>Recommended %</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {budgetsArray.map((item) => {
+                                return (
+                                    <tr key={item.id}>
+                                        <td>{item.category.name}</td>
+                                        <td>{item.dollar_amount}</td>
+                                        {/* <td
+                                            onClick={() =>
+                                                setUpdateBool(!updateBool)
+                                            }
+                                        >
+                                            Update
+                                        </td> */}
+                                        <td>{item.personal_percent}</td>
+                                        <td>
+                                            {item.category.recommended_percent}
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                    <button onClick={() => setUpdateBool(!updateBool)}>
+                        Update
+                    </button>
+                </div>
             )}
         </div>
     );
